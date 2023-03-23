51250a8f334561c027a2eacba169f2d9ff84d95c
Akshay Aileni <akshayakshay5543@gmail.com>
2023-03-23 14:54:53 +0530

    feat:back to normal icons

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,48 +1,49 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import node from "../../Assets/gifs/node.gif";
-import mongo from "../../Assets/gifs/mongo.gif";
-import git from "../../Assets/gifs/git.gif";
-import selenium from "../../Assets/gifs/selenium.png";
-import css from "../../Assets/gifs/css.gif";
-import react from "../../Assets/gifs/react.gif";
-import python from "../../Assets/gifs/python.gif";
-import js from "../../Assets/gifs/js.gif";
-import tensor from "../../Assets/gifs/tensor.gif";
-import sql from "../../Assets/gifs/sql.gif";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPython,
+  DiGit,
+  DiHtml5,
+  DiStackoverflow,
+} from "react-icons/di";
+import { SiPytorch, SiFirebase, SiNextdotjs } from "react-icons/si";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <img src={node} width="100px" alt="Node" />
+        <DiHtml5 />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <img src={mongo} width="120px" alt="MongoDB" />
+        <DiJavascript1 />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <img src={git} width="150px" alt="Git" />
+        <DiNodejs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <img src={selenium} width="90px" alt="Selenium" />
+        <DiReact />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <img src={css} width="100px" alt="css" />
+        <DiMongodb />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <img src={sql} width="120px" alt="SQl" />
+        <SiNextdotjs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <img src={react} width="100px" alt="React" />
+        <DiGit />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <img src={python} width="100px" alt="Python" />
+        <SiFirebase />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <img src={js} width="100px" alt="JS" />
+        <DiPython />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <img src={tensor} width="100px" alt="Tensor" />
+        <DiStackoverflow />
       </Col>
     </Row>
   );
